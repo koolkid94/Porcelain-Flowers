47e191c8dd7a314e050ec2ee91aee97dbf58a097
koolkid94 <alexander.yuchi@gmail.com>
2024-06-25 16:29:13 -0700

    initial mod setup

---
+++ src/main/java/net/kuwulkid/porcelain/PorcelainFlowersClient.java
@@ -0,0 +1,10 @@
+package net.kuwulkid.porcelain;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PorcelainFlowersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/kuwulkid/porcelain/PorcelainFlowersDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kuwulkid.porcelain;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PorcelainFlowersDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
