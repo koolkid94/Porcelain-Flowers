7bf952e509c88727a57a0bc00bd2a934ad480ce5
koolkid94 <alexander.yuchi@gmail.com>
2024-06-26 01:38:50 -0700

    minor tweaks

--- src/main/java/net/kuwulkid/porcelain/entity/custom/JungleLaborerEntity.java
+++ src/main/java/net/kuwulkid/porcelain/entity/custom/JungleLaborerEntity.java
@@ -1,10 +1,7 @@
 package net.kuwulkid.porcelain.entity.custom;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.goal.LookAroundGoal;
-import net.minecraft.entity.ai.goal.LookAtEntityGoal;
-import net.minecraft.entity.ai.goal.SwimGoal;
-import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.MobEntity;
@@ -23,8 +20,11 @@ import software.bernie.geckolib.animation.*;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class JungleLaborerEntity extends VillagerEntity implements GeoEntity {
+    int output, numOne = 0, numTwo = 4;
     protected static final RawAnimation WALK_ANIM = RawAnimation.begin().thenLoop("animation.villager.walk");
     protected static final RawAnimation IDLE_ANIM = RawAnimation.begin().thenLoop("animation.villager.idle");
+    protected static final RawAnimation IDLE_ANIM2 = RawAnimation.begin().thenLoop("animation.villager.fishing");
+    protected static final RawAnimation SWIM_ANIM = RawAnimation.begin().thenLoop("animation.villager.swim");
 
     private final AnimatableInstanceCache geoCache = GeckoLibUtil.createInstanceCache(this);
    public JungleLaborerEntity(EntityType<? extends VillagerEntity> entityType, World world) {
@@ -42,27 +42,35 @@ public class JungleLaborerEntity extends VillagerEntity implements GeoEntity {
 
     public static DefaultAttributeContainer.Builder createJungleLaborerAttributes(){
         return MobEntity.createMobAttributes()
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 3)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED,0.21);
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 20)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED,0.5);
 
     }
 
 
     protected <E extends JungleLaborerEntity> PlayState walkAnimController(final AnimationState<E> event) {
-        if (event.isMoving()) {
+
+        output = (int) ((numTwo - numOne + 1) * Math.random() + numOne);
+        if (event.isMoving() && !isWet()) {
             return event.setAndContinue(WALK_ANIM);
         }
-        else
+        else if(isTouchingWater())
         {
-            return event.setAndContinue(IDLE_ANIM);
+            return event.setAndContinue(SWIM_ANIM);
         }
+        else if((output == 3) && !event.isMoving())
+        {
+            return event.setAndContinue(IDLE_ANIM2);
+        }
+        else
+            return event.setAndContinue(IDLE_ANIM);
 
     }
 
     protected void initGoals() {
         this.goalSelector.add(0, new SwimGoal(this));
-        //this.goalSelector.add(2, new ProjectileAttackGoal(this, 1.0, 60, 10.0F));
-        this.goalSelector.add(2, new WanderAroundFarGoal(this, 1.0));
+        this.goalSelector.add(1, new EscapeDangerGoal(this, 1.1));
+        this.goalSelector.add(2, new WanderAroundFarGoal(this, 1.1));
         this.goalSelector.add(3, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
         this.goalSelector.add(4, new LookAroundGoal(this));
         this.goalSelector.add(5, new LookAtEntityGoal(this, MobEntity.class, 8.0F));
