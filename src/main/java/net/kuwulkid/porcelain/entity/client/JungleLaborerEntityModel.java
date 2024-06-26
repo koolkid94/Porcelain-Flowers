ebdb8a823b855af4dd4f38f3d512fcd4d2660cd8
koolkid94 <alexander.yuchi@gmail.com>
2024-06-26 15:55:13 -0700

    Removed Mixin

--- src/main/java/net/kuwulkid/porcelain/entity/client/JungleLaborerEntityModel.java
+++ src/main/java/net/kuwulkid/porcelain/entity/client/JungleLaborerEntityModel.java
@@ -3,9 +3,27 @@ package net.kuwulkid.porcelain.entity.client;
 import net.kuwulkid.porcelain.PorcelainFlowers;
 import net.kuwulkid.porcelain.entity.custom.JungleLaborerEntity;
 import net.minecraft.util.Identifier;
+import software.bernie.geckolib.animation.AnimationState;
+import software.bernie.geckolib.animation.AnimationState;
+import software.bernie.geckolib.cache.object.GeoBone;
+import software.bernie.geckolib.constant.DataTickets;
+import software.bernie.geckolib.loading.json.raw.Bone;
 import software.bernie.geckolib.model.GeoModel;
+import software.bernie.geckolib.model.data.EntityModelData;
 
 public class JungleLaborerEntityModel extends GeoModel<JungleLaborerEntity> {
+
+    @Override
+    public void setCustomAnimations(JungleLaborerEntity animatable, long instanceId, AnimationState<JungleLaborerEntity> state) {
+        super.setCustomAnimations(animatable, instanceId, state);
+
+        EntityModelData extraDataOfType = state.getData(DataTickets.ENTITY_MODEL_DATA);
+        var head = this.getAnimationProcessor().getBone("head");
+        if (head != null) {
+            head.setRotY(extraDataOfType.netHeadYaw() * ((float)Math.PI / 180F));
+        }
+    }
+
 
     @Override
     public Identifier getModelResource(JungleLaborerEntity object) {
